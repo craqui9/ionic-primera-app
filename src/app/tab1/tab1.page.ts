2f767e5d7c84eeb831d5e41abe94baac944fca8a
craqui9 <daniru9@gmail.com>
2020-09-07 19:12:07 +0200

    Fin seccion 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { disableDebugTools } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(){
+    console.log('hola mundo');
+    
+    let numero = 1;
+
+    numero = numero + 12;
+
+    debugger;
+
+    numero = numero - 1;
+
+    numero = 0;
+
+    console.log(numero);
+    
+
+  }
 
 }
